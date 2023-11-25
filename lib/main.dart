4d6743358fa89e108eee9f4e79d1c68df8212559
Andreas Monitzer <andreas@monitzer.com>
2023-11-25 18:10:46 +0100

    Fixed the title (that isn't shown anyways)

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class DashboardApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Dashboard',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSwatch(
